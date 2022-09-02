5019be56793896d817f714f8c13911d57170a103
Raphael Zimmermann <mail@raphael.li>
2022-09-02 15:39:21 +0200

    Improve installer

--- install.bash
+++ install.bash
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-echo "Ensure you run ./gradlew build beforehand!"
+read -r -p "Ensure you run ./gradlew build beforehand. [proceed] " _
 
-# udev rule to simplify access to device + user permissions
+echo "Installing udev rule to simplify access to device and to ensure user permissions"
 sudo cp 10-rfid-reader.rules /etc/udev/rules.d/10-rfid-reader.rules
 sudo udevadm control --reload
 
+read -r -p "(Re-)connect RFID-Reader [proceed] " _
+
+echo "Installing user systemd service"
 if [[ -f ~/.config/systemd/user/rfid-az.service ]]; then
   rm ~/.config/systemd/user/rfid-az.service
 fi
 ln -s "$(pwd)/rfid-az.service" ~/.config/systemd/user/rfid-az.service
+systemctl --user daemon-reload
 systemctl --user enable rfid-az.service
+systemctl --user restart rfid-az.service
+systemctl --user status rfid-az.service
 
-echo "reconnect RFID reader to test"
-echo "Then run systemctl --user restart rfid-az.service"
+echo "Done"
